02a48a0e97895beb3f1fa4ce84eba77b9dcefdea
Fardeen Khan <khanfardeen9983@gmail.com>
2019-05-15 21:29:47 +0530

    TernaryOperator

---
+++ Assets/Scripts/Ternaryoperator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ternaryoperator : MonoBehaviour
+{
+    private int clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clip = Random.Range(10, 101);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool x = clip > 0 ? true : false;
+        if (x)
+            //Shoot
+            clip--;
+        else
+            //Reload
+            clip = Random.Range(10, 101);
+
+        Debug.Log("Clip : " + clip);
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Benefit of using Properties instead of private properties is that 
 1. Through combinations of getter and setter we can create either read-only, write-only or both types of properties.
 2. Getters and setter can have other lines of code opening up to using coroutines.
+### Ternary Operators
+Acts on three operands.
+1. A condition
+2. The set of statements to execute if the condition is true
+3. The set of statements to execute if the condition is false
+
+Example:
+```c#
+int clip = 10;
+string msg = clip > 0 ? "Clip ready" : "Reload!!";
